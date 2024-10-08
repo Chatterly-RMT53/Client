1bcda033219abdb4020ca7f4e7648a44aa0ae7ac
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-09 07:24:02 +0800

    feat: use auth context provider to main wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,13 @@ import { createRoot } from "react-dom/client";
 import router from "./config/router";
 import "./styles/index.css";
 import { Toaster } from "@/components/ui/toaster";
+import AuthProvider from "./contexts/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-    <Toaster />
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster />
+    </AuthProvider>
   </StrictMode>,
 );
