e406cf86e03b41ba92e7bc8b7fbda9f258db08f4
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-09 07:08:45 +0800

    feat: add toaster

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { createRoot } from "react-dom/client";
 
 import router from "./config/router";
 import "./styles/index.css";
+import { Toaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    <Toaster />
   </StrictMode>,
 );
