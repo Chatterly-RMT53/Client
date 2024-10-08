ec6deccbd1586146d84ddb360248c3d948332636
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-09 07:25:51 +0800

    feat: add anonymous sign in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,24 @@ import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { Link } from "react-router-dom";
 
+import initialize from "@/config/firebase";
+import { signInAnonymously } from "firebase/auth";
+import { useToast } from "@/hooks/useToast";
+
 export default function Login() {
+  const { auth } = initialize();
+  const { toast } = useToast();
+
+  const anonymousSignIn = () => {
+    signInAnonymously(auth).catch(() => {
+      toast({
+        title: "Failed",
+        description: "Failed to sign in anonymously",
+        variant: "destructive",
+      });
+    });
+  };
+
   return (
     <main
       className="justify-content-center flex h-[100vh] items-center"
@@ -42,20 +59,24 @@ export default function Login() {
           <Button className="w-full" type="submit">
             Login
           </Button>
-          <div className="my-8 flex w-full items-center justify-center gap-x-4">
-            <Separator className="flex-1" />
-            <p className="text-sm text-neutral-600">Or Continue With</p>
-            <Separator className="flex-1" />
-          </div>
-          <div className="flex gap-x-4">
-            <Button className="w-full" variant="outline" asChild>
-              <Link to="/register">Register</Link>
-            </Button>
-            <Button className="w-full" variant="outline">
-              Anonymous
-            </Button>
-          </div>
         </form>
+        <div className="my-8 flex w-full items-center justify-center gap-x-4">
+          <Separator className="flex-1" />
+          <p className="text-sm text-neutral-600">Or Continue With</p>
+          <Separator className="flex-1" />
+        </div>
+        <div className="flex gap-x-4">
+          <Button className="w-full" variant="outline" asChild>
+            <Link to="/register">Register</Link>
+          </Button>
+          <Button
+            className="w-full"
+            variant="outline"
+            onClick={() => anonymousSignIn()}
+          >
+            Anonymous
+          </Button>
+        </div>
       </Card>
     </main>
   );
