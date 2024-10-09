dc8efaff4fd73da2f67eab429e105e41bbfc7e80
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-10 05:00:45 +0800

    feat: add create room

--- src/components/chat/RoomList.jsx
+++ src/components/chat/RoomList.jsx
@@ -7,12 +7,25 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import initialize from "@/config/firebase";
-import { collection, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+import { useToast } from "@/hooks/useToast";
 
 export default function RoomList({ roomId, setRoomId }) {
+  const { toast } = useToast();
   const { firestore } = initialize();
   const [rooms, setRooms] = useState([]);
+  const [newRoom, setNewRoom] = useState("");
   const collectionRooms = collection(firestore, "rooms");
 
   useEffect(() => {
@@ -30,6 +43,43 @@ export default function RoomList({ roomId, setRoomId }) {
 
   return (
     <>
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="mb-4 w-full">Create Room</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create Room</DialogTitle>
+              <DialogDescription>Create a new room</DialogDescription>
+            </DialogHeader>
+            <div>
+              <label>Name</label>
+              <Input
+                placeholder="Enter the room name"
+                value={newRoom}
+                onChange={(event) => setNewRoom(event.target.value)}
+              />
+              <div className="flex justify-end">
+                <Button
+                  className="mt-4"
+                  onClick={() => {
+                    addDoc(collectionRooms, { name: newRoom }).catch((e) =>
+                      toast({
+                        title: "Error",
+                        description: "Failed to create a new room",
+                        variant: "destructive",
+                      }),
+                    );
+                  }}
+                >
+                  Create
+                </Button>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
       <div className="w-full space-y-4">
         {rooms.map((r) => (
           <Card
