ef631fe340955237acb5ea2bc0b721552e326d6f
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-10 11:10:11 +0800

    fix: add scroll list to rooms list

--- src/components/chat/RoomList.jsx
+++ src/components/chat/RoomList.jsx
@@ -18,8 +18,11 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogClose,
 } from "@/components/ui/dialog";
+
 import { Input } from "@/components/ui/input";
+import { ScrollArea } from "../ui/scroll-area";
 
 export default function RoomList({ roomId, setRoomId }) {
   const { toast } = useToast();
@@ -41,6 +44,20 @@ export default function RoomList({ roomId, setRoomId }) {
     });
   }, []);
 
+  const createRoom = (e) => {
+    e.preventDefault(e);
+
+    addDoc(collectionRooms, { name: newRoom }).catch(() =>
+      toast({
+        title: "Error",
+        description: "Failed to create a new room",
+        variant: "destructive",
+      }),
+    );
+
+    setNewRoom("");
+  };
+
   return (
     <>
       <div>
@@ -54,47 +71,40 @@ export default function RoomList({ roomId, setRoomId }) {
               <DialogDescription>Create a new room</DialogDescription>
             </DialogHeader>
             <div>
-              <label>Name</label>
-              <Input
-                placeholder="Enter the room name"
-                value={newRoom}
-                onChange={(event) => setNewRoom(event.target.value)}
-              />
-              <div className="flex justify-end">
-                <Button
-                  className="mt-4"
-                  onClick={() => {
-                    addDoc(collectionRooms, { name: newRoom }).catch((e) =>
-                      toast({
-                        title: "Error",
-                        description: "Failed to create a new room",
-                        variant: "destructive",
-                      }),
-                    );
-                  }}
-                >
-                  Create
-                </Button>
-              </div>
+              <form onClick={createRoom}>
+                <label>Name</label>
+                <Input
+                  placeholder="Enter the room name"
+                  value={newRoom}
+                  onChange={(event) => setNewRoom(event.target.value)}
+                />
+                <div className="flex justify-end">
+                  <DialogClose>
+                    <Button className="mt-4">Create</Button>
+                  </DialogClose>
+                </div>
+              </form>
             </div>
           </DialogContent>
         </Dialog>
       </div>
-      <div className="w-full space-y-4">
-        {rooms.map((r) => (
-          <Card
-            className="cursor-pointer rounded-md"
-            key={r.id}
-            onClick={() => {
-              setRoomId(r.id);
-            }}
-          >
-            <CardHeader>
-              <CardTitle>{r.name}</CardTitle>
-            </CardHeader>
-          </Card>
-        ))}
-      </div>
+      <ScrollArea>
+        <div className="h-[65vh] w-full space-y-4">
+          {rooms.map((r) => (
+            <Card
+              className="cursor-pointer rounded-md"
+              key={r.id}
+              onClick={() => {
+                setRoomId(r.id);
+              }}
+            >
+              <CardHeader>
+                <CardTitle>{r.name}</CardTitle>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
+      </ScrollArea>
     </>
   );
 }
